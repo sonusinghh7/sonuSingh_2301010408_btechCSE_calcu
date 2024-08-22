1e969274e05cb43c759bbe0b44972f89b22d3c5b
Sonu Singh <100152492+sonusinghh7@users.noreply.github.com>
2024-08-22 10:13:52 +0530

    Add files via upload

---
+++ calculater/script.js
@@ -0,0 +1,31 @@
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function deleteLast() {
+    const display = document.getElementById('display');
+    display.value = display.value.slice(0, -1);
+}
+
+function appendNumber(number) {
+    document.getElementById('display').value += number;
+}
+
+function appendOperator(operator) {
+    const display = document.getElementById('display');
+    const lastChar = display.value.slice(-1);
+    if (['+', '-', '*', '/'].includes(lastChar)) {
+        display.value = display.value.slice(0, -1) + operator;
+    } else {
+        display.value += operator;
+    }
+}
+
+function calculate() {
+    const display = document.getElementById('display');
+    try {
+        display.value = eval(display.value);
+    } catch (e) {
+        display.value = 'Error';
+    }
+}
